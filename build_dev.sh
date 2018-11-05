3d6004664ae4cd9f4d5fbe4c94673677beda250f
Russel <russel@Tixguru8de-MacBook-Pro.local>
2018-11-05 11:45:39 +0800

    update fix script

--- build_dev.sh
+++ build_dev.sh
@@ -1,3 +1,3 @@
 cp -R ./config/config.development.toml ./config.toml
 go build main.go
-pmgo start $GOPATH/src/github.com/utranslator-server
+pmgo start $GOPATH/src/github.com/utranslator-server zimu-server
